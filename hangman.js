f3f1ad95a941b33e68d05d109c87006c923eeb78
Keith Prifte <kprift@gmail.com>
2016-02-21 16:00:14 -0500

    total rewrite based off hw review and Jesse's solution

--- hangman.js
+++ hangman.js
@@ -1,76 +1,29 @@
 //Welcome Message
 console.log("welcome to hangman");
-// Prompts user to get secret word --> UNCOMMENT WHEN DONE <-- secretWord = prompt("Please select a secret word!")
+// Prompts user to get secret word --> UNCOMMENT WHEN DONE <--
 
-  //Hardcoded secretWord, delete when done.
-  var secretWord = 'toast'
-  console.log('secretWord = ' + secretWord)
+secretWord = prompt("Please select a secret word!")
 
-secretWordHidden = []
-letterGuesses = []
+//Hardcoded secretWord, delete when done.var secretWord = 'toast'
 
-secretWordArray = secretWord.split('');
+letterGuesses = [] ;
+var correctGuesses = []
 
-
-// Function to show user the amount of letters in the secret word
-function spaces(){
-  for (i = 0; i < secretWord.length; i++) {
-    secretWordHidden.push('_');
-  }
-} spaces()
-
-//Loop to start the letter guessing protocol
-for (i = 0; (i < (secretWord.length)); i++) {
-  //Executes prompt
-  letterGuesses.push(prompt('Word:  ' + secretWordHidden.join(' ')+ '\n' + 'Please guess a letter.  Guesses left: ' + ((secretWord.length + 1)- i)))
-  console.log(i + ' time running ; letterGuessed = ' + letterGuesses[i] + 'letterGuesses = ' + letterGuesses)
-  //2nd loop to evaluate if the guess was correct
-  if (secretWord.includes(letterGuesses[i]) === true) {
-      //If the letter guessed is in the secretWord, the 1st IF statement evulates here to the 2nd loop
-      for (j = 0; j < secretWord.length; j++) {
-      // 2nd loop is needed to check the guess against ALL letters of the secretWord
-      if (letterGuesses[i] === secretWord[j]) {
-        //IF the guess is the same as ANY letter in the secretWord, we then overwrite the secretWordHidden array which is all underscores ("_")
-        secretWordHidden[j] = letterGuesses[i]
-        detailedCorrectConsoleMessage()
-        console.log('detailedCorrectConsoleMessage')
-        //end of nested IF statement
-      } else detailedWrongConsoleMessage()
-        console.log('detailedWrongConsoleMessage');
-  //1st loop evulates here once if runs though the nested if and 2nd for loop
-  }  correctMessage()
-    console.log(i + 'correctMessage()');
-  } else if (secretWord.includes(letterGuesses[i]) !== true) {
-    incorrectMessage()
-    console.log(i + ' secretWord.includes(letterGuesses[i]) !== true');
-  } else if (secretWordHidden === secretWord) {
-    alert('You Win!' + '\n' + 'Word:  ' + secretWordHidden.join(' '))
+while (letterGuesses.length < (secretWord.length + 3)) {
+  letterGuesses.push(prompt(displaySecretWord(secretWord) + "     Pick a letter."));
+  if (displaySecretWord(secretWord) === secretWord) {
+    alert('You win!')
+    break;
+  } else if (letterGuesses.length === (secretWord.length + 3)) {
+    alert('You lose')
   }
 }
 
-//End of IF statement.  Beginning of Message functions:
-function firstGuessPrompt() {
-  letterGuesses.push(prompt('Word:  ' + secretWordHidden.join(' ')+ '\n' + 'Please guess a letter.  Guesses left: ' + ((secretWord.length + 1)- i)))
-}
-
-function incorrectMessage() {
-  letterGuesses.push(prompt('INCORRECT!!!' + '\n' + 'Word:  ' + secretWordHidden.join(' ')+ '\n' + 'Please guess a letter.  Guesses left: ' + ((secretWord.length + 3)- i)))
-}
-
-function detailedCorrectConsoleMessage() {
-  console.log('Correct.  i = ' + i + ', j = ' + j + ', letterGuesses[i] = ' + letterGuesses[i] + ', secretWord[j] = ' + secretWord[j] + ', and secretWordHidden = ' + secretWordHidden)
-}
-
-function detailedWrongConsoleMessage() {
-  console.log('Wrong.  i = ' + i + ', j = ' + j + ', letterGuesses[i] = ' + letterGuesses[i] + ', secretWord[j] = ' + secretWord[j] + ', and secretWordHidden = ' + secretWordHidden)
-}
-
-function correctMessage() {
-  letterGuesses.push(prompt('CORRECT!!!' + '\n' + 'Word:  ' + secretWordHidden.join(' ')+ '\n' + 'Please guess a letter.  Guesses left: ' + ((secretWord.length + 1)- i)));
-}
-
-
-//correctGuess = letterGuesses.push(prompt('Word: ' + secretWordHidden.join(' ')))
-
-
-//prompt('Word: ' + secretWordHidden.join(' ')+ '\n' + 'You have ' + 'guesses');
+function displaySecretWord(secretWord){
+  display = ""
+for (i = 0; i < secretWord.length; i++) {
+    if (letterGuesses.includes(secretWord[i])) {
+      display = display + secretWord[i];
+    } else display = display + ' _ ';
+  } return display
+};
